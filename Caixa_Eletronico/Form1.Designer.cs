9ec4fcfc0095889f92657ddd18f0eac95aa80a51
ra2457066 <ra2457066@LDI316>
2025-02-26 20:34:59 -0300

    Add project files.

---
+++ Caixa_Eletronico/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Caixa_Eletronico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.btDepositar = new System.Windows.Forms.Button();
+            this.btSacar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.domainUpDown2 = new System.Windows.Forms.DomainUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(37, 51);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(74, 25);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "Valor:";
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.domainUpDown1.Location = new System.Drawing.Point(117, 53);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(207, 26);
+            this.domainUpDown1.TabIndex = 1;
+            this.domainUpDown1.Text = "domainUpDown1";
+            // 
+            // btDepositar
+            // 
+            this.btDepositar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDepositar.Location = new System.Drawing.Point(42, 116);
+            this.btDepositar.Name = "btDepositar";
+            this.btDepositar.Size = new System.Drawing.Size(138, 46);
+            this.btDepositar.TabIndex = 2;
+            this.btDepositar.Text = "Depositar";
+            this.btDepositar.UseVisualStyleBackColor = true;
+            // 
+            // btSacar
+            // 
+            this.btSacar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSacar.Location = new System.Drawing.Point(201, 116);
+            this.btSacar.Name = "btSacar";
+            this.btSacar.Size = new System.Drawing.Size(138, 46);
+            this.btSacar.TabIndex = 3;
+            this.btSacar.Text = "Sacar";
+            this.btSacar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Saldo:";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(131, 198);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(94, 29);
+            this.lblSaldo.TabIndex = 5;
+            this.lblSaldo.Text = "R$ 0.00";
+            // 
+            // domainUpDown2
+            // 
+            this.domainUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.domainUpDown2.Location = new System.Drawing.Point(125, 263);
+            this.domainUpDown2.Name = "domainUpDown2";
+            this.domainUpDown2.Size = new System.Drawing.Size(207, 26);
+            this.domainUpDown2.TabIndex = 6;
+            this.domainUpDown2.Text = "domainUpDown2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Limite:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 340);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.domainUpDown2);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSacar);
+            this.Controls.Add(this.btDepositar);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.lblValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Button btDepositar;
+        private System.Windows.Forms.Button btSacar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.DomainUpDown domainUpDown2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
